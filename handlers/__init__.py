d124643b0937c0df0ddcde9b22c8c132d18e9773
diff <pronagibstor07@gmail.com>
2026-02-28 10:30:29 +0200

    v3

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,6 +3,7 @@ from .authorization import router as auth_router
 from .pc_control import router as pc_router
 from .wol import router as wol_router
 from .dota import router as dota_router
+from .voice import router as voice_router
 from .notifications import router as notification_router
 
 __all__ = [
@@ -10,5 +11,6 @@ __all__ = [
     "pc_router",
     "wol_router",
     "dota_router",
+    "voice_router",
     "notification_router",
 ]
